1411e426cc374642f11229208f9c7b8a5072cad0
rafalL7 <rafal.sarnecki@gum.gov.pl>
2022-02-21 14:48:52 +0100

    Initial commit

---
+++ src/main/java/NowyUser.java
@@ -0,0 +1,16 @@
+
+
+public class NowyUser
+{
+    String name;
+    String surname;
+
+    public String getName() {
+        return name;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/NowyUserTest.java
@@ -0,0 +1,28 @@
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class NowyUserTest {
+
+    @Test
+    public void testInit(){
+        NowyUser user = new NowyUser();
+        user.setName("Rafał Sarnecki");
+        String name;
+        name = user.getName();
+        System.out.println(name);
+    }
+
+    @Before
+    public void setUp() {
+        this.testInit();
+    }
+
+    @After
+    public void tearDown() {
+       System.out.println("It works");
+    }
+}
